2a87b10c0e8952875dce8c76bb18f6cb9dda2e7a
Lincoln Laylor <lincolnlaylor@gmail.com>
2026-09-10 08:25:42 -0400

    fix: escape angle brackets in the structured data as intended

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default async function Page() {
       <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{
-          __html: JSON.stringify(jsonLd).replace(/</g, "\u003c"),
+          __html: JSON.stringify(jsonLd).replace(/</g, "\\u003c"),
         }}
       />
       <SiteHeader stars={stars} download={download} />
